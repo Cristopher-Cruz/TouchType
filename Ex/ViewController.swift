5cea7c5cfa869f57e2af8266ede79a17c097d87f
Chriscruzz17 <christophercruz0236@gmail.com>
2023-04-07 02:32:10 -0400

    Initial Commit

---
+++ Ex/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Ex
+//
+//  Created by christopher cruz on 4/7/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
