f446e456d0a8075ff96189c943c99020a857c59c
Chriscruzz17 <christophercruz0236@gmail.com>
2023-04-09 18:18:21 -0400

    Fixed All keyboard View bugs

--- Ex/KeyboardView.swift
+++ Ex/KeyboardView.swift
@@ -2,31 +2,70 @@ import Cocoa
 
 class KeyboardView: NSView {
     // An array of buttons representing the keys on the keyboard
-    var keyButtons = [NSButton]()
+    var keyButtons = [[NSButton]]()
     
-    // The current character index to highlight in blue
-    var currentCharIndex = 0 {
-        didSet {
-            // Highlight the key corresponding to the current character index in blue
-            if currentCharIndex < keyButtons.count {
-                let button = keyButtons[currentCharIndex]
-                button.layer?.backgroundColor = NSColor.blue.cgColor
-            }
-        }
-    }
+
     
     // The practice line string to synchronize with the keyboard view
     var practiceLine = "" {
         didSet {
             // Reset the keyboard view to unhighlight all keys
-            for button in keyButtons {
-                button.layer?.backgroundColor = NSColor.clear.cgColor
+            for row in keyButtons {
+                for button in row {
+                    button.layer?.backgroundColor = NSColor.clear.cgColor
+                }
             }
             
             // Set the current character index to 0
             currentCharIndex = 0
         }
     }
+    
+    // The current character index to highlight in blue
+
+    var count = 0
+    var currentCharIndex = 0 {
+        didSet {
+            // Unhighlight the old current button
+            currentButton?.layer?.backgroundColor = NSColor.clear.cgColor
+            
+            // Highlight the key corresponding to the new current character index in blue
+            let currchar = String(practiceLine[practiceLine.index(practiceLine.startIndex, offsetBy: currentCharIndex)])
+            let (rowIndex, colIndex) = getButtonRowAndColumnForCurrentChar(char: currchar)
+            let button = keyButtons[rowIndex][colIndex]
+            button.layer?.backgroundColor = NSColor.blue.cgColor
+            
+            // Set the current button to the new button
+            currentButton = button
+        }
+    }
+
+
+    
+    func getButtonRowAndColumnForCurrentChar(char: String) -> (Int, Int) {
+        let currentChar = String(practiceLine[practiceLine.index(practiceLine.startIndex, offsetBy: currentCharIndex)])
+        let rowsOfChars = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+                           ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+                           ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+                           ["z", "x", "c", "v", "b", "n", "m"],
+                           ["", " ", ""]]
+        for (rowIndex, rowChars) in rowsOfChars.enumerated() {
+            for (colIndex, tempstr) in rowChars.enumerated() {
+                if tempstr == currentChar {
+                    if char == currentChar {
+                        print("found it")
+                        return (rowIndex, colIndex)
+                    } else if char == tempstr.uppercased() {
+                        print("found it")
+                        return (rowIndex, colIndex)
+                    }
+                }
+            }
+        }
+        return (0,0)
+    }
+
+
     
     // The current button in the keyboard view
     var currentButton: NSButton? {
@@ -35,7 +74,7 @@ class KeyboardView: NSView {
             oldValue?.layer?.backgroundColor = NSColor.clear.cgColor
             
             // Highlight the new current button
-            currentButton?.layer?.backgroundColor = NSColor.yellow.cgColor
+            currentButton?.layer?.backgroundColor = NSColor.blue.cgColor
         }
     }
     
@@ -56,27 +95,22 @@ class KeyboardView: NSView {
         var yPos: CGFloat = buttonPadding
         
         for rowChars in rowsOfChars {
-            for char in rowChars {
-                let button = NSButton(frame: NSRect(x: xPos, y: yPos, width: buttonWidth, height: buttonHeight))
-                button.title = char
-                button.bezelStyle = .rounded
-                button.layer?.backgroundColor = NSColor.clear.cgColor
-                addSubview(button)
-                keyButtons.append(button)
-                xPos += buttonWidth + buttonPadding
-            }
-            yPos += buttonHeight + buttonPadding
-            xPos = buttonPadding
-        }
-        
-        // Set the initial state of the keyboard to unhighlight all keys
-        for button in keyButtons {
-            button.layer?.backgroundColor = NSColor.clear.cgColor
-        }
-        
-        // Set the current button to the first button in the keyboard view
-        currentButton = keyButtons.first
-    }
+              var rowButtons = [NSButton]()
+              for char in rowChars {
+                  let button = NSButton(frame: NSRect(x: xPos, y: yPos, width: buttonWidth, height: buttonHeight))
+                  button.title = char
+                  button.bezelStyle = .rounded
+                  button.layer?.backgroundColor = NSColor.clear.cgColor
+                  addSubview(button)
+                  rowButtons.append(button)
+
+                  xPos += buttonWidth + buttonPadding
+              }
+              keyButtons.append(rowButtons)
+              xPos = buttonPadding
+              yPos += buttonHeight + buttonPadding
+          }
+      }
     
     
     override func keyDown(with event: NSEvent) {
@@ -85,16 +119,11 @@ class KeyboardView: NSView {
         
         // Check if the typed character matches the current character in the practice string
         if typedChar == String(practiceLine[practiceLine.index(practiceLine.startIndex, offsetBy: currentCharIndex)]) {
+            let (rowIndex, colIndex) = getButtonRowAndColumnForCurrentChar(char: typedChar)
+            let button = keyButtons[rowIndex][colIndex]
             // Increment the current character index
             currentCharIndex += 1
-            
-            // Set the current button to the next button in the keyboard
-
-            // Set the current button to the next button in the keyboard view
-            if let currentIndex = keyButtons.firstIndex(of: currentButton!), currentIndex + 1 < keyButtons.count {
-                currentButton = keyButtons[currentIndex + 1]
-            }
+    
         }
     }
 }
-
